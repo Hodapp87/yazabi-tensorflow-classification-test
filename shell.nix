b1d0f5627c570d0dd9892d0533a4137787c1ad6e
Chris Hodapp <hodapp87@gmail.com>
2017-11-15 14:28:33 -0500

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let stdenv = pkgs.stdenv;
+    optional = stdenv.lib.optional;
+    tf = pkgs.python35Packages.tensorflowWithCuda;
+    #tf_objdet = pkgs.python35Packages.tensorflow_object_detection.override {
+    #    tensorflow = tf;
+    #};
+    python_with_deps = pkgs.python35.withPackages
+      (ps: [ps.cython ps.scipy tf ps.matplotlib ps.opencv3
+            ps.easydict ps.pillow ps.pyyaml
+            ps.pyqt4 # Needed only for matplotlib backend
+            ps.pycallgraph ps.graphviz
+            ps.jupyter
+            #tf_objdet
+            ]);
+in stdenv.mkDerivation rec {
+  name = "yazabi-tensorflow-skill-test";
+
+  buildInputs = with pkgs; [ python_with_deps ];
+}
